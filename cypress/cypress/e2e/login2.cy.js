8d899c396b742d919f064c9f3436e45ee77f736e
Eduardo-Manoel <eduardomanoelnn@yahoo.com.br>
2022-08-23 09:54:34 -0300

    main

--- cypress/cypress/e2e/login2.cy.js
+++ cypress/cypress/e2e/login2.cy.js
@@ -1,10 +1,20 @@
-/// <reference types="Cypress" />
-
-describe('Login', function () {
+describe('SEU BARRIGA', function () {
+  beforeEach(function () {
+    cy.visit('/')
+  })
+})
+it('COMEÇO- Verifica o título da aplicação', function () {
+  cy.title()
+    .should('be.equal', 'Seu Barriga - Log in')
+})
+it('Verifica o Titulo de baixo', function () {
+  cy.get('a:contains(Seu Barriga)')
+    .should('be.visible')
+    .click()
+})
+describe('Login', () => {
   it.only('successfully', () => {
     cy.Login()
-    cy.get('.btn')
-      .click()
     cy.contains('Seu Barriga')
       .should('exist')
   })
--- cypress/cypress/support/logincommands.js
+++ cypress/cypress/support/logincommands.js
@@ -1,7 +1,11 @@
 /// <reference types="Cypress" />
 
-Cypress.Commands.add('Login', function() {
-    cy.visit('/')
-    cy.get("#email").type(Cypress.env('user_name'))
-    cy.get("#senha").type(Cypress.env('user_password')) 
-  })
+Cypress.Commands.add('Login', function () {
+  cy.visit('/')
+  cy.get("#email")
+    .type(Cypress.env('user_name'))
+  cy.get("#senha")
+    .type(Cypress.env('user_password'))
+  cy.get('.btn')
+    .click()
+})
